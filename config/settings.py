c71c989eabeb52739fd362fe6851b9ddf887f850
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-09-09 05:07:46 +0500

    poetry add pillow

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django_extensions",
     "drf_yasg",
 
+    "users",
+
 ]
 
 REST_FRAMEWORK = {
@@ -109,10 +111,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"
 
-# LOGIN_REDIRECT_URL = "/"
-# LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
