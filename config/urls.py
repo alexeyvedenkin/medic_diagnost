f3e70013d2f6729503cd29bca7344d79e3dd4908
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-09-26 00:35:03 +0500

    создание приложения main

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+class ServiceListView:
+    pass
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("", ServiceListView.as_view(), name="homepage"),
     path("users/", include("users.urls", namespace="users")),
 
 ]
